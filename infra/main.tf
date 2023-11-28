79df26c7bea449a19b7acc7dc46de6dc725d5724
Emirton Sousa <emirtonf@gmail.com>
2023-11-28 15:12:27 -0300

    feat: load balancer resources created.

--- infra/main.tf
+++ infra/main.tf
@@ -26,12 +26,47 @@ resource "aws_launch_template" "terraform-template-ec2" {
 }
 
 resource "aws_autoscaling_group" "autoscaling-group" {
-  availability_zones = [ "${var.region_aws}a" ]
+  availability_zones = [ "${var.region_aws}a", "${var.region_aws}b" ]
   name = var.group_name
   max_size = var.max_size
   min_size = var.min_size
   launch_template {
     id = aws_launch_template.terraform-template-ec2.id
     version = "$Latest"
+  }
+  target_group_arns = aws_lb_target_group.lb_target_group.arn
+}
+
+resource "aws_default_subnet" "subnet_1" {
+  availability_zone = "${var.region_aws}a"
+}
+
+resource "aws_default_subnet" "subnet_2" {
+  availability_zone = "${var.region_aws}b"
+}
+
+resource "aws_lb" "load_balancer" {
+  internal = false
+  subnets = [ aws_default_subnet.subnet_1, aws_default_subnet.subnet_2 ]
+}
+
+resource "aws_lb_target_group" "lb_target_group" {
+  name = "target_instances"
+  port = "8000"
+  protocol = "HTTP"
+  vpc_id = aws_vpc.vpc_default.id
+}
+
+resource "aws_vpc" "vpc_default" {
+  
+}
+
+resource "aws_lb_listener" "listener_lb" {
+  load_balancer_arn = aws_lb.load_balancer.arn
+  port = "8000"
+  protocol = "HTTP"
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.lb_target_group.arn 
   }
 }
